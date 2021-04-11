74e3d31f3466ed26a6deb0f5361fe343538f3ff4
Stevility <65314652+Stevility@users.noreply.github.com>
2021-04-11 13:28:40 -0400

    Update Randomizer

--- Randomizer.java
+++ Randomizer.java
@@ -14,23 +14,18 @@ public class Randomizer
 
 
     /**
-     * nextInt is a static method designed to return a random number between 1 and maxValue, inclusive
-     * NOTE this is different than the behavior of the Random class default behavior which
-     * returns 0 .. maxValue-1
-     * 
-     * As a side effect, the first time the Randomizer is utilized it initializes 
-     * the object of type Random. It is implementing the Singleton pattern. We will discuss singletons
-     * later in the course
-     * 
-     * @param  maxValue  the cap of the random number generation (inclusive)
-     * @return a random number from 1..maxvalue
+     * The constructor for the classes.
      */
-    public static int nextInt(int maxValue)
+    public Randomizer ()
     {
-        if (rand == null) {
-            rand = new Random();
-        }
-        
-        return rand.nextInt(maxValue)+1;
+        rand = new Random();
+    }
+    
+    /**
+     * Returns an int.
+     */
+    public static int nextInt(int x)
+    {
+        return rand.nextInt(x);
     }
 }
